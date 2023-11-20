6f23b2597493c42884ed2df0b19e113420a7fa7c
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-20 20:21:12 +0100

    function 02

--- js/script.js
+++ js/script.js
@@ -7,42 +7,42 @@
 // Dichiariamo chi ha vinto.
 
 
-// (usando una funzione 01
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-//  funzione 02
-function isEvenOrOdd(num) {
-    if (sum % 2 === 0) {
-        return "pari" ;
-    } else {
-        return "dispari" ;
-    }
-}
+// usando una funzione 01
+// function randomIntFromInterval(min, max) { // min and max included 
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+//   }
+// //  funzione 02
+// function isEvenOrOdd(num) {
+//     if (sum % 2 === 0) {
+//         return "pari" ;
+//     } else {
+//         return "dispari" ;
+//     }
+// }
 
-// L’utente sceglie pari o dispari
-const inOddEven = prompt("Inserisci pari o dispari");
-console.log('hai scelto '+inOddEven)
-// e inserisce un numero da 1 a 5.
-const inUser = parseInt(prompt("Inserisci un numero da 1 a 5"));
-console.log('hai inserito ' + inUser);
-// Generiamo un numero random (sempre da 1 a 5) per il computer 
-const rndInt = randomIntFromInterval(1, 6)
-console.log( 'Numero generato dal PC '+ rndInt)
+// // L’utente sceglie pari o dispari
+// const inOddEven = prompt("Inserisci pari o dispari");
+// console.log('hai scelto '+inOddEven)
+// // e inserisce un numero da 1 a 5.
+// const inUser = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// console.log('hai inserito ' + inUser);
+// // Generiamo un numero random (sempre da 1 a 5) per il computer 
+// const rndInt = randomIntFromInterval(1, 6)
+// console.log( 'Numero generato dal PC '+ rndInt)
 
-//  Sommiamo i due numeri
-const sum = inUser + rndInt
-console.log("Somma " + sum)
+// //  Sommiamo i due numeri
+// const sum = inUser + rndInt
+// console.log("Somma " + sum)
 
-//  Stabiliamo se la somma dei due numeri è pari o dispari 
-const isEven = isEvenOrOdd(sum)
+// //  Stabiliamo se la somma dei due numeri è pari o dispari 
+// const isEven = isEvenOrOdd(sum)
 
-// Dichiariamo chi ha vinto.
-if (inOddEven === isEven ) {
-    alert("Hai vinto!")
-} else {
-    alert ("Non hai vinto!")
-}
+// // Dichiariamo chi ha vinto.
+// if (inOddEven === isEven ) {
+//     alert("Hai vinto!")
+// } else {
+//     alert ("Non hai vinto!")
+// }
 
 
 
@@ -53,16 +53,16 @@ if (inOddEven === isEven ) {
 
 
 
+function fastestIsPalindrome(str) {
+    let len = Math.floor(str.length / 2);
+    for (var i = 0; i < len; i++)
+      if (str[i] !== str[str.length - i - 1])
+        return "is not a palindrome";
+    return 'is a palindrome';
+  }
 
+ 
+  const result = fastestIsPalindrome(prompt("Inserisci una parola"));
+  console.log(result) ;
 
-
-
-
-
-
-
-
-
-
-
-
+  
